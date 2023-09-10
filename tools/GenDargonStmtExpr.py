890c029cde3ff8955f7b451ffaef47f9c9708220
Kyle Morris <54403410+kylemmorris@users.noreply.github.com>
2023-09-10 14:04:00 -0400

    Added new tools directory and python script WIP

---
+++ tools/GenDargonStmtExpr.py
@@ -0,0 +1,99 @@
+# Dargon Programming Language 
+# (C) Kyle Morris 2023
+
+# This utility will be used to generate the expression and statement structures.
+# Output: Expr.h
+
+from io import TextIOWrapper
+
+### ------------###
+### DEFINITIONS ###
+### ------------###
+
+# The file name
+fname = './Expr.h'
+
+# The input expressions
+exprs = [\
+    'Binary: Expr* left, Token op, Expr* right',\
+    #'Literal: void* value'\
+]
+
+### ------------###
+###  FUNCTIONS  ###
+### ------------###
+
+# Prints the copywrite info
+def printCopywrite(f: TextIOWrapper):
+    cwrite = ['/*\n',\
+                 ' * Dargon Programming Language\n',\
+                 ' * (C) Kyle Morris 2023 - See LICENSE.txt for license information\n',\
+                 ' * \n',\
+                 ' * FILE: Expr.h\n',\
+                 ' * \n',\
+                 ' * DESCRIPTION: Expressions.\n',\
+                 ' * \n',\
+                 ' * SINCE: v0.1\n',\
+                 ' * \n',\
+                 ' */ \n']
+    f.writelines(cwrite)
+
+# Prints the base class
+def printBaseClass(f: TextIOWrapper, name: str):
+    f.write('#ifndef DARGON_EXPR_H\n')
+    f.write('#define DARGON_EXPR_H\n\n')
+    f.write('#include <string>\n\n')
+    f.write('namespace dargon {\n\n')
+    comment = ['\t/**\n',\
+              '\t * @brief The base class of all Expressions.\n',\
+              '\t *\n',\
+              '\t*/\n'\
+              ]
+    f.writelines(comment)
+    f.write(f'\tstruct {name} ')
+    f.write('{ };\n\n')
+
+def printGetter(f: TextIOWrapper, type: str, name: str):
+    f.write(f'const {type}& ')
+
+# Creates a new subtype
+def createExprSubclass(f: TextIOWrapper, base: str, name: str, fields: list[str]):
+    f.write(f'\tstruct {name} : public {base} ')
+    f.write('{\n\tpublic:\n')
+    f.write(f'\t\t{name}(')
+    firsts = fields[:len(fields)-1]
+    end = fields[len(fields)-1]
+    print(firsts)
+    print(end)
+    for field in firsts:
+        vtype = field.strip().split()[0]
+        vname = field.strip().split()[1]
+        f.write(f'{vtype} {vname}, ')
+    if end != []:
+        vtype = end.strip().split()[0]
+        vname = end.strip().split()[1]
+        f.write(f'{vtype} {vname}')
+    f.write(') {}\n')
+    for field in fields:
+        vtype = field.strip().split()[0]
+        vname = field.strip().split()[1]
+        printGetter(f,vtype, vname)
+    f.write('\t};\n')
+
+# Prints the ending stuff
+def printEnd(f: TextIOWrapper):
+    f.write('\n};\n#endif')
+
+### ------------###
+### MAIN        ###
+### ------------###
+
+with open(fname, 'w') as fref:
+    printCopywrite(fref)
+    base = 'Expr'
+    printBaseClass(fref, base)
+    for expr in exprs:
+        name = expr.split(':')[0]
+        fields = expr.split(':')[1].split(',')
+        createExprSubclass(fref, base, name, fields)
+    printEnd(fref)
